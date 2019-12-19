bd0f5d7f0e9e7912de599c45356210411a315e8a
ashraj1 <ashraj1@live.com>
2019-12-18 22:24:51 -0500

    Update startup-geoserver-azure-web-app.sh

--- 2.14/startup-geoserver-azure-web-app.sh
+++ 2.14/startup-geoserver-azure-web-app.sh
@@ -2,7 +2,7 @@
 set -ex
 ssh-keygen -A
 service ssh restart
-#if [ ! -d "$GEOSERVER_DATA_DIR" ]; then
-#    cp -r $GEOSERVER_HOME/data_dir $GEOSERVER_DATA_DIR
-#fi
+if [ ! -d "$GEOSERVER_DATA_DIR" ]; then
+    cp -r $GEOSERVER_HOME/data_dir $GEOSERVER_DATA_DIR
+fi
 /startup-geoserver-base.sh
